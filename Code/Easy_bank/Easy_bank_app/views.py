b59d48ed4990dbcc3403fb5faf143511a775d6ba
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2021-05-01 01:58:08 +0600

    Enhancement #12 : signup & login view created

--- Code/Easy_bank/Easy_bank_app/views.py
+++ Code/Easy_bank/Easy_bank_app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from . import forms
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def home_view(request):
+  return render(request,'Easy_bank_app/homebase.html')
+
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return HttpResponseRedirect('adminlogin')
+
+
+def customerclick_view(request):
+    
+    return render(request, 'Easy_bank_app/userlogin.html')
+
+
+def customer_signup_view(request):
+    userForm=forms.CustomerUserForm()
+    customerForm=forms.CustomerForm()     
+    easy_bank_app={'userForm':userForm,'customerForm':customerForm}
+    if request.method=='POST':
+        userForm=forms.CustomerUserForm(request.POST)
+        customerForm=forms.CustomerForm(request.POST,request.FILES)
+        if userForm.is_valid() and customerForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            customer=customerForm.save(commit=False)
+            customer.user=user
+            customer.save()
+            my_user_group = Group.objects.get_or_create(name='CUSTOMER')
+            my_user_group[0].user_set.add(user)
+        return HttpResponseRedirect('customerlogin')
+    
+    return render(request,'Easy_bank_app/usersignup.html',context=easy_bank_app)
