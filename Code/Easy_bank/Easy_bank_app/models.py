81571b0a9a5672d095df25422ba76c13c70eda5e
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2021-05-01 01:57:30 +0600

    Enhancement #15 #16 : table created
    Loan and credit card table created

--- Code/Easy_bank/Easy_bank_app/models.py
+++ Code/Easy_bank/Easy_bank_app/models.py
@@ -14,3 +14,19 @@ class Customer(models.Model):
         return self.user.id
     def __str__(self):
         return self.user.first_name
+    
+class Loan(models.Model):
+    types_of_loan = models.CharField(max_length=40, null=False)
+    bank_name= models.CharField(max_length=70, null=False)
+    interest_rate = models.CharField(max_length=30,null=False)
+    processing_fee_range = models.CharField(max_length=40)
+    loan_amount= models.CharField(max_length=50, null=False)
+    tenure_range= models.CharField(max_length=70, null=False)
+    
+class Credit_card(models.Model):  
+    bank_name = models.CharField(max_length=50)
+    card_type= models.CharField(max_length=50)
+    first_year_fee= models.CharField(max_length=50)
+    rewards= models.CharField(max_length=50)
+    joining_perks= models.CharField(max_length=50)
+
