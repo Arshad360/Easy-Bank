918d046bcbc5367d408b26645eb4a82bdab6d7ba
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2021-05-01 19:41:05 +0600

    Enhancement #4 : psycopg extension added

--- Code/Easy_bank/Easy_bank/settings.py
+++ Code/Easy_bank/Easy_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import psycopg2.extensions
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,7 +88,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 
 # Password validation
