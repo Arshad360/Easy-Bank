dcf31824b5b0b161923b0e4050f6dcc81d9e719d
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2021-06-03 16:42:53 +0600

    Enhancement #18: views created for contact us form

--- Code/Easy_bank/Easy_bank_app/views.py
+++ Code/Easy_bank/Easy_bank_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from . import forms
 from django.http import HttpResponseRedirect
 from . import models
+from django.contrib import messages
 
 # Create your views here.
 def home_view(request):
@@ -50,10 +51,7 @@ def showdata_credit(request):
 
 def slider_view(request):
     return render(request,'Easy_bank_app/slider.html')
-
-def contactus_view(request):
-    return render(request,'Easy_bank_app/contact_us.html')
-
+ 
 def compare_view(request):
     return render(request,'Easy_bank_app/compareandapply.html')
 
@@ -74,3 +72,17 @@ def contactus_sent_view_2(request):
             contactusForm.save()
             return render(request, '')
     return render(request, 'Easy_bank_app/contact.html', {'contactusForm':contactusForm})
+
+def contactus_view(request):
+    if request.method=='POST':
+        if request.POST.get('name') and request.POST.get('email') and request.POST.get('phone') and request.POST.get('message'):
+            saverecord=models.Contactus()
+            saverecord.name=request.POST.get('name')
+            saverecord.email=request.POST.get('email')
+            saverecord.phone=request.POST.get('phone')
+            saverecord.message=request.POST.get('message')
+            saverecord.save()
+            messages.success(request,'Record Saved Successfully!!!!!')
+            return render(request,'Easy_bank_app/contact_us.html')
+    else:
+            return render(request,'Easy_bank_app/contact_us.html')
