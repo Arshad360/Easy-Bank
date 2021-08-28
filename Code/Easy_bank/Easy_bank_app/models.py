54c51ee688cc00d1649fbf209d5095ae1eb108fa
Hasanul Banna <hasangituses@gmail.com>
2021-08-29 02:46:35 +0600

    updated

--- Code/Easy_bank/Easy_bank_app/models.py
+++ Code/Easy_bank/Easy_bank_app/models.py
@@ -208,4 +208,19 @@ class brac_credit_card_form(models.Model):
     
     class meta:
         db_table="easy_bank_app_brac_credit_card_form"
+    
+class city_credit_card_form1(models.Model):
+    applicants_name = models.CharField(max_length=250)
+    applicants_full_name= models.CharField(max_length=200)
+    applicants_father_name= models.CharField(max_length=200)
+    applicants_mother_name= models.CharField(max_length=200)
+    nationality= models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    contatct_no= models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    nid = models.CharField(max_length=100)
+    loan_type= models.CharField(max_length=200)
+    
+    class meta:
+        db_table="easy_bank_app_city_credit_card_form1"
 
