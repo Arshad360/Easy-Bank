2f4b40a0669b707f6dbe15f1d3f02346ab3ad042
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2021-09-04 02:51:49 +0600

    update

--- Code/Easy_bank/Easy_bank_app/models.py
+++ Code/Easy_bank/Easy_bank_app/models.py
@@ -481,69 +481,6 @@ class brac_education_loan_form5(models.Model):
     class meta:
         db_table="easy_bank_app_brac_education_loan_form5"
 
-class brac_credit_card_form1(models.Model):
-    applicants_name = models.CharField(max_length=250)
-    applicants_full_name= models.CharField(max_length=200)
-    applicants_father_name= models.CharField(max_length=200)
-    applicants_mother_name= models.CharField(max_length=200)
-    nationality= models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
-    contatct_no= models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    nid = models.CharField(max_length=100)
-    credit_card_type= models.CharField(max_length=200)
-    
-    class meta:
-        db_table="easy_bank_app_brac_credit_card_form1"
-
-class brac_credit_card_form2(models.Model):
-    full_address= models.CharField(max_length=200)
-    city= models.CharField(max_length=100)
-    postal_code= models.CharField(max_length=100)
-    p_address= models.CharField(max_length=200)
-    second_contact_no= models.CharField(max_length=100)
-    second_email= models.CharField(max_length=100)
-    
-    class meta:
-        db_table="easy_bank_app_brac_credit_card_form2"
-
-class brac_credit_card_form3(models.Model):
-    organisation_name= models.CharField(max_length=100)
-    designation_department= models.CharField(max_length=100)
-    office_address= models.CharField(max_length=200)
-    allowness= models.CharField(max_length=100,null=True)
-    additional_income= models.CharField(max_length=100,null=True)
-    salary_total= models.CharField(max_length=100,null=True)
-    office_no= models.CharField(max_length=100)
-    
-    class meta:
-        db_table="easy_bank_app_brac_credit_card_form3"
-
-class brac_credit_card_form4(models.Model):
-    applicants_name = models.CharField(max_length=250)
-    applicants_full_name= models.CharField(max_length=200)
-    applicants_father_name= models.CharField(max_length=200)
-    applicants_mother_name= models.CharField(max_length=200)
-    nationality= models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
-    contatct_no= models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    nid = models.CharField(max_length=100)
-    credit_card_type= models.CharField(max_length=200)
-    
-    class meta:
-        db_table="easy_bank_app_brac_credit_card_form4"
-
-class brac_credit_card_form5(models.Model): 
-    account_no= models.CharField(max_length=100)
-    account_type= models.CharField(max_length=100)
-    balance_amount= models.CharField(max_length=200)
-    contact_no_2= models.CharField(max_length=100,null=True)
-    email= models.CharField(max_length=100,null=True)
-    
-    class meta:
-        db_table="easy_bank_app_brac_credit_card_form5"
-
 class city_credit_card_form1(models.Model):
     applicants_name = models.CharField(max_length=250)
     applicants_full_name= models.CharField(max_length=200)
@@ -607,6 +544,69 @@ class city_credit_card_form5(models.Model):
     
     class meta:
         db_table="easy_bank_app_city_credit_card_form5"
+
+class brac_credit_card_form1(models.Model):
+    applicants_name = models.CharField(max_length=250)
+    applicants_full_name= models.CharField(max_length=200)
+    applicants_father_name= models.CharField(max_length=200)
+    applicants_mother_name= models.CharField(max_length=200)
+    nationality= models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    contatct_no= models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    nid = models.CharField(max_length=100)
+    credit_card_type= models.CharField(max_length=200)
+    
+    class meta:
+        db_table="easy_bank_app_brac_credit_card_form1"
+
+class brac_credit_card_form2(models.Model):
+    full_address= models.CharField(max_length=200)
+    city= models.CharField(max_length=100)
+    postal_code= models.CharField(max_length=100)
+    p_address= models.CharField(max_length=200)
+    second_contact_no= models.CharField(max_length=100)
+    second_email= models.CharField(max_length=100)
+    
+    class meta:
+        db_table="easy_bank_app_brac_credit_card_form2"
+
+class brac_credit_card_form3(models.Model):
+    organisation_name= models.CharField(max_length=100)
+    designation_department= models.CharField(max_length=100)
+    office_address= models.CharField(max_length=200)
+    allowness= models.CharField(max_length=100,null=True)
+    additional_income= models.CharField(max_length=100,null=True)
+    salary_total= models.CharField(max_length=100,null=True)
+    office_no= models.CharField(max_length=100)
+    
+    class meta:
+        db_table="easy_bank_app_brac_credit_card_form3"
+
+class brac_credit_card_form4(models.Model):
+    applicants_name = models.CharField(max_length=250)
+    applicants_full_name= models.CharField(max_length=200)
+    applicants_father_name= models.CharField(max_length=200)
+    applicants_mother_name= models.CharField(max_length=200)
+    nationality= models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    contatct_no= models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    nid = models.CharField(max_length=100)
+    credit_card_type= models.CharField(max_length=200)
+    
+    class meta:
+        db_table="easy_bank_app_brac_credit_card_form4"
+
+class brac_credit_card_form5(models.Model): 
+    account_no= models.CharField(max_length=100)
+    account_type= models.CharField(max_length=100)
+    balance_amount= models.CharField(max_length=200)
+    contact_no_2= models.CharField(max_length=100,null=True)
+    email= models.CharField(max_length=100,null=True)
+    
+    class meta:
+        db_table="easy_bank_app_brac_credit_card_form5"
 
 
 class city_home_loan_form1(models.Model): 
